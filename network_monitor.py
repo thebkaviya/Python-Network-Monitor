f2618bc336d243ad879b0156408b391225786c0f
Binula Kavisinghe <bkavisinghe@gmail.com>
2022-10-05 17:31:17 +0530

    Update network_monitor.py

--- network_monitor.py
+++ network_monitor.py
@@ -14,26 +14,30 @@ def get_size(bytes):
             return f"{bytes:.2f}{unit}B"
         bytes /= 1024
 
-# get the network I/O stats from psutil on each network interface
-io = psutil.net_io_counters(pernic=True)
+def full_mode():
 
-while True:
+    # get the network I/O stats from psutil on each network interface
+    io = psutil.net_io_counters(pernic=True)
 
-    time.sleep(UPDATE_DELAY)
-    io_2 = psutil.net_io_counters(pernic=True)  # get the network I/O stats again per interface
-    
-    data = []
-    for iface, iface_io in io.items():
-        # new - old stats gets us the speed
-        upload_speed, download_speed = io_2[iface].bytes_sent - iface_io.bytes_sent, io_2[iface].bytes_recv - iface_io.bytes_recv
-        data.append({
-            "iface": iface, "Download": get_size(io_2[iface].bytes_recv),
-            "Upload": get_size(io_2[iface].bytes_sent),
-            "Upload Speed": f"{get_size(upload_speed / UPDATE_DELAY)}/s",
-            "Download Speed": f"{get_size(download_speed / UPDATE_DELAY)}/s",
-        })
-    io = io_2 # update the I/O stats
-    df = pd.DataFrame(data) # construct a Pandas DataFrame to print stats in a tabular style
-    df.sort_values("Download", inplace=True, ascending=False) # sort values per column
-    os.system("cls") if "nt" in os.name else os.system("clear") # clear the screen
-    print(df.to_string())
+    while True:
+
+        time.sleep(UPDATE_DELAY)
+        io_2 = psutil.net_io_counters(pernic=True)  # get the network I/O stats again per interface
+        
+        data = []
+        for iface, iface_io in io.items():
+            # new - old stats gets us the speed
+            upload_speed, download_speed = io_2[iface].bytes_sent - iface_io.bytes_sent, io_2[iface].bytes_recv - iface_io.bytes_recv
+            data.append({
+                "iface": iface, "Download": get_size(io_2[iface].bytes_recv),
+                "Upload": get_size(io_2[iface].bytes_sent),
+                "Upload Speed": f"{get_size(upload_speed / UPDATE_DELAY)}/s",
+                "Download Speed": f"{get_size(download_speed / UPDATE_DELAY)}/s",
+            })
+        io = io_2 # update the I/O stats
+        df = pd.DataFrame(data) # construct a Pandas DataFrame to print stats in a tabular style
+        df.sort_values("Download", inplace=True, ascending=False) # sort values per column
+        os.system("cls") if "nt" in os.name else os.system("clear") # clear the screen
+        print(df.to_string())
+
+full_mode()
